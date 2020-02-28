8d56e46f53cd877dd320352eb6d2920ab28c1bfc
Yaroslav Panasenko <yar.panasenko@gmail.com>
2020-02-28 18:47:56 +0200

    Fix registration

---
+++ Example/Tests/TokenDSDK_IntegrationTests/IntegrationTestsConstants.swift
@@ -0,0 +1,15 @@
+//
+//  IntegrationTestsConstants.swift
+//  TokenDSDK_IntegrationTests
+//
+//  Created by Yaroslav on 2/28/20.
+//  Copyright © 2020 CocoaPods. All rights reserved.
+//
+
+import Foundation
+
+struct IntegrationTestsConstants {
+    public static let apiUrlString = "http://localhost:8000/_/api/"
+    public static let email = "test@dl.com"
+    public static let password = "test@dl.com"
+}
