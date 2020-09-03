84e87d50c2e4f2e4e4103720bcbffda9b70f972c
Yehor Miroshnychenko <mierosh@gmail.com>
2020-09-03 13:24:23 +0300

    Added blob attachment initializer

--- Sources/v1/Common/APIs/Blobs/BlobResponse.swift
+++ Sources/v1/Common/APIs/Blobs/BlobResponse.swift
@@ -157,6 +157,17 @@ extension BlobResponse.BlobContent {
         public let mimeType: String
         public let name: String
         public let key: String
+
+        public init(
+            mimeType: String,
+            name: String,
+            key: String
+        ) {
+
+            self.mimeType = mimeType
+            self.name = name
+            self.key = key
+        }
     }
 }
 
