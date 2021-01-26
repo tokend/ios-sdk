7558dd1d446e5865028821c04697dcd583c881a3
Andrey Karpenko <53317161+Coconut022@users.noreply.github.com>
2021-01-26 20:27:18 +0200

    Added other filter to SchedulerApiV3

--- Sources/JSONAPI/v3/APIs/Scheduler/SchedulerRequestFilterV3.swift
+++ Sources/JSONAPI/v3/APIs/Scheduler/SchedulerRequestFilterV3.swift
@@ -3,11 +3,7 @@ import Foundation
 public enum SchedulerRequestFilterOptionV3: FilterOption {
     
     case payload(_ value: String)
-    case startTime(_ value: Int)
-    case endTime(_ value: Int)
+    case other(_ value: [String: String])
 }
 
 public class SchedulerRequestFiltersV3: RequestFilters<SchedulerRequestFilterOptionV3> {}
-
-// Other filters.
-//case other(_ value: [String: String])
