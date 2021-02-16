d14ea840feca2a4e7766107a9746731020353736
Andrey Karpenko <53317161+Coconut022@users.noreply.github.com>
2021-02-16 18:29:51 +0200

    Added missing requests to VaccineResultsApiV3

--- Sources/JSONAPI/v3/APIs/VaccineResults/VaccineResultsApiV3.swift
+++ Sources/JSONAPI/v3/APIs/VaccineResults/VaccineResultsApiV3.swift
@@ -37,7 +37,7 @@ public class VaccineResultsApiV3: JSONAPI.BaseApi {
         
         let cancelable = self.network.getEmptyCancelable()
         
-        self.requestBuilder.buildVaccineListResults(
+        self.requestBuilder.buildVaccineListResultsRequest(
             filters: filters,
             include: include,
             pagination: pagination,
@@ -60,6 +60,141 @@ public class VaccineResultsApiV3: JSONAPI.BaseApi {
                             
                         case .failure(let error):
                             completion(.failure(error))
+                        }
+                    }
+                )
+            }
+        )
+        
+        return cancelable
+    }
+    
+    /// Method sends request to fetch vaccine result's data from api.
+    /// - Parameters:
+    ///   - bookingId: Identifier of booking for which data will be fetched.
+    ///   - completion: The block which is called when the result will be fetched.
+    ///   - result: The model of `RequestSingleResult<MunaVaccineResults.VaccineResource>`
+    /// - Returns: `Cancelable`
+    @discardableResult
+    public func getVaccineResultById(
+        vaccineResultId: String,
+        completion: @escaping (_ result: RequestSingleResult<MunaVaccineResults.VaccineResource>) -> Void
+    ) -> Cancelable {
+        
+        let cancelable = self.network.getEmptyCancelable()
+
+        self.requestBuilder.buildVaccineResultByIdRequest(
+            vaccineResultId: vaccineResultId,
+            completion: { [weak self] (request) in
+                
+                guard let request = request else {
+                    completion(.failure(JSONAPIError.failedToSignRequest))
+                    return
+                }
+                
+                cancelable.cancelable = self?.requestSingle(
+                    MunaVaccineResults.VaccineResource.self,
+                    request: request,
+                    completion: { (result) in
+                        switch result {
+                        
+                        case .failure(let error):
+                            completion(.failure(error))
+                            
+                        case .success(let document):
+                            completion(.success(document))
+                        }
+                    }
+                )
+            }
+        )
+        
+        return cancelable
+    }
+    
+    /// Method sends request to fetch vaccine types from api.
+    /// - Parameters:
+    ///   - filters: Request filters.
+    ///   - pagination: Pagination option.
+    ///   - completion: The block which is called when the result will be fetched.
+    ///   - result: The model of `RequestCollectionResult<MunaVaccineResults.VaccineTypeResource>`
+    /// - Returns: `Cancelable`
+    @discardableResult
+    public func getVaccineTypesList(
+        filters: VaccineResultsRequestFiltersV3,
+        include: [String]?,
+        pagination: RequestPagination,
+        completion: @escaping ((_ result: RequestCollectionResult<MunaVaccineResults.VaccineTypeResource>) -> Void)
+    ) -> Cancelable {
+        
+        let cancelable = self.network.getEmptyCancelable()
+        
+        self.requestBuilder.buildVaccineTypesListRequest(
+            filters: filters,
+            include: include,
+            pagination: pagination,
+            completion: { [weak self] (request) in
+                
+                guard let request = request else {
+                    completion(.failure(JSONAPIError.failedToSignRequest))
+                    return
+                }
+                
+                cancelable.cancelable = self?.requestCollection(
+                    MunaVaccineResults.VaccineTypeResource.self,
+                    request: request,
+                    completion: { (result) in
+                        
+                        switch result {
+                        
+                        case .success(let document):
+                            completion(.success(document))
+                            
+                        case .failure(let error):
+                            completion(.failure(error))
+                        }
+                    }
+                )
+            }
+        )
+        
+        return cancelable
+    }
+    
+    /// Method sends request to fetch vaccine type's data from api.
+    /// - Parameters:
+    ///   - bookingId: Identifier of booking for which data will be fetched.
+    ///   - completion: The block which is called when the result will be fetched.
+    ///   - result: The model of `RequestSingleResult<MunaVaccineResults.VaccineTypeResource>`
+    /// - Returns: `Cancelable`
+    @discardableResult
+    public func getVaccineTypeById(
+        vaccineTypeId: String,
+        completion: @escaping (_ result: RequestSingleResult<MunaVaccineResults.VaccineTypeResource>) -> Void
+    ) -> Cancelable {
+        
+        let cancelable = self.network.getEmptyCancelable()
+
+        self.requestBuilder.buildVaccineTypeByIdRequest(
+            vaccineTypeId: vaccineTypeId,
+            completion: { [weak self] (request) in
+                
+                guard let request = request else {
+                    completion(.failure(JSONAPIError.failedToSignRequest))
+                    return
+                }
+                
+                cancelable.cancelable = self?.requestSingle(
+                    MunaVaccineResults.VaccineTypeResource.self,
+                    request: request,
+                    completion: { (result) in
+                        switch result {
+                        
+                        case .failure(let error):
+                            completion(.failure(error))
+                            
+                        case .success(let document):
+                            completion(.success(document))
                         }
                     }
                 )
--- Sources/JSONAPI/v3/APIs/VaccineResults/VaccineResultsRequestBuilderV3.swift
+++ Sources/JSONAPI/v3/APIs/VaccineResults/VaccineResultsRequestBuilderV3.swift
@@ -10,7 +10,8 @@ public class VaccineResultsRequestBuilderV3: JSONAPI.BaseApiRequestBuilder {
     private var vaccineResults: String { "vaccine-results" }
     private var vaccineType: String { "vaccine-type" }
 
-    public func buildVaccineListResults(
+    /// Builds request to get vaccine results list
+    public func buildVaccineListResultsRequest(
         filters: VaccineResultsRequestFiltersV3,
         include: [String]?,
         pagination: RequestPagination,
@@ -29,6 +30,74 @@ public class VaccineResultsRequestBuilderV3: JSONAPI.BaseApiRequestBuilder {
                 queryParameters: queryParameters,
                 include: include,
                 pagination: pagination
+            ),
+            shouldSign: true,
+            sendDate: sendDate,
+            completion: completion
+        )
+    }
+    
+    /// Builds request to get vaccine result
+    /// - Parameters:
+    ///   - vaccineResultId: The vaccine result id.
+    public func buildVaccineResultByIdRequest(
+        vaccineResultId: String,
+        sendDate: Date = Date(),
+        completion: @escaping (JSONAPI.RequestModel?) -> Void
+    ) {
+        let path = /self.integrations/self.vaccineResults/vaccineResultId
+        
+        self.buildRequest(
+            JSONAPI.BaseRequestBuildModel.simple(
+                path: path,
+                method: .get
+            ),
+            shouldSign: true,
+            sendDate: sendDate,
+            completion: completion
+        )
+    }
+    
+    /// Builds request to get vaccine types list
+    public func buildVaccineTypesListRequest(
+        filters: VaccineResultsRequestFiltersV3,
+        include: [String]?,
+        pagination: RequestPagination,
+        sendDate: Date = Date(),
+        completion: @escaping (JSONAPI.RequestModel?) -> Void
+    ) {
+        let path = /self.integrations/self.vaccineResults/self.vaccineType
+        
+        let queryParameters = self.buildFilterQueryItems(filters.filterItems)
+        
+        self.buildRequest(
+            .simpleQueryIncludePagination(
+                path: path,
+                method: .get,
+                queryParameters: queryParameters,
+                include: include,
+                pagination: pagination
+            ),
+            shouldSign: true,
+            sendDate: sendDate,
+            completion: completion
+        )
+    }
+    
+    /// Builds request to get vaccine result
+    /// - Parameters:
+    ///   - vaccineTypeId: The vaccine result id.
+    public func buildVaccineTypeByIdRequest(
+        vaccineTypeId: String,
+        sendDate: Date = Date(),
+        completion: @escaping (JSONAPI.RequestModel?) -> Void
+    ) {
+        let path = /self.integrations/self.vaccineResults/self.vaccineType/vaccineTypeId
+        
+        self.buildRequest(
+            JSONAPI.BaseRequestBuildModel.simple(
+                path: path,
+                method: .get
             ),
             shouldSign: true,
             sendDate: sendDate,
