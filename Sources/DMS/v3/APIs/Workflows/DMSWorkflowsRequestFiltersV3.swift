95719f9fa56e295f3f9e9a0ceac55a897267fe80
Yehor Miroshnychenko <yehor.miroshnychenko.dl@gmail.com>
2021-04-08 17:49:16 +0300

    Fixed DMSWorkflowsRequestFiltersV3

--- Sources/DMS/v3/APIs/Workflows/DMSWorkflowsRequestFiltersV3.swift
+++ Sources/DMS/v3/APIs/Workflows/DMSWorkflowsRequestFiltersV3.swift
@@ -4,7 +4,7 @@ public extension DMS {
     
     enum WorkflowsRequestFilterOptionV3: FilterOption {
         
-        case projectId(_ value: String)
+        case projectId(_ value: Int)
         case userId(_ value: String)
         case documentNo(_ value: String)
         case documentTitle(_ value: String)
