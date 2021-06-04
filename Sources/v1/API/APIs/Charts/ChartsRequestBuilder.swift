c6f25d6425b07bd58424d772bf556ec33eabebff
Yehor Miroshnychenko <yehor.miroshnychenko.dl@gmail.com>
2021-06-04 17:00:45 +0300

    Refactored ChartsApi

--- Sources/v1/API/APIs/Charts/ChartsRequestBuilder.swift
+++ Sources/v1/API/APIs/Charts/ChartsRequestBuilder.swift
@@ -4,21 +4,29 @@ import Foundation
 /// which are used to fetch charts
 public class ChartsRequestBuilder: BaseApiRequestBuilder {
     
-    // MARK: - Public
+    // MARK: - Private properties
+    
+    private let charts: String = "charts"
+    
+}
+    
+// MARK: - Public methods
+
+public extension ChartsRequestBuilder {
     
     /// Builds request to fetch charts for according asset from api
     /// - Parameters:
     ///   - asset: The code of the asset for which charts are going to be fetched.
     ///   - sendDate: Send time of request.
     ///   - completion: Returns `ChartsRequest` or nil.
-    public func buildChartsRequest(
+    func buildChartsRequest(
         asset: String,
         sendDate: Date,
-        completion: @escaping (ChartsRequest?) -> Void
+        completion: @escaping (RequestSigned?) -> Void
         ) {
         
         let baseUrl = self.apiConfiguration.urlString
-        let url = baseUrl.addPath("charts").addPath(asset)
+        let url = baseUrl/self.charts/asset
         
         self.buildRequestSigned(
             baseUrl: baseUrl,
