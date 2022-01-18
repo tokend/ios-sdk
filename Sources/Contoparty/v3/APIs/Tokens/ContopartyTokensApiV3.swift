074937d3f861d7a60960ec6b2d793d882e521bbf
Yurii Poberezhnyi <yurapoberezhnyy@gmail.com>
2022-01-18 15:31:27 +0200

    added getTokenById()

--- Sources/Contoparty/v3/APIs/Tokens/ContopartyTokensApiV3.swift
+++ Sources/Contoparty/v3/APIs/Tokens/ContopartyTokensApiV3.swift
@@ -66,4 +66,42 @@ public extension Contoparty.TokensApiV3 {
         
         return cancelable
     }
+    
+    @discardableResult
+    func getTokenById(
+        id: String,
+        completion: @escaping ((_ result: RequestSingleResult<Contoparty.TokenResource>) -> Void)
+    ) -> Cancelable {
+        
+        let cancelable = self.network.getEmptyCancelable()
+        
+        self.requestBuilder.buildGetTokenById(
+            id: id,
+            completion: { [weak self] (request) in
+                
+                guard let request = request else {
+                    completion(.failure(JSONAPIError.failedToSignRequest))
+                    return
+                }
+                
+                cancelable.cancelable = self?.requestSingle(
+                    Contoparty.TokenResource.self,
+                    request: request,
+                    completion: { (result) in
+                        
+                        switch result {
+                        
+                        case .failure(let error):
+                            completion(.failure(error))
+                            
+                        case .success(let document):
+                            completion(.success(document))
+                        }
+                    }
+                )
+            }
+        )
+        
+        return cancelable
+    }
 }
