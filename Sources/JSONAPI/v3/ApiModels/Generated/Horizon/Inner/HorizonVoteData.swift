945a67a72cdebfbcced4a0b9e3663ecf22e6d24e
Ksenia Didusenko <ksenia.didusenko@gmail.com>
2022-06-21 23:47:43 +0300

    Updated resources

--- Sources/JSONAPI/v3/ApiModels/Generated/Horizon/Inner/HorizonVoteData.swift
+++ Sources/JSONAPI/v3/ApiModels/Generated/Horizon/Inner/HorizonVoteData.swift
@@ -9,16 +9,12 @@ public struct VoteData: Decodable {
     
     public enum CodingKeys: String, CodingKey {
         // attributes
-        case creationTime
-        case customChoice
         case pollType
         case singleChoice
     }
     
     // MARK: Attributes
     
-    public let creationTime: Date?
-    public let customChoice: String?
     public let pollType: Enum
     public let singleChoice: UInt64?
 
@@ -27,8 +23,6 @@ public struct VoteData: Decodable {
     public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         
-        self.creationTime = try? container.decodeOptionalDateString(key: .creationTime)
-        self.customChoice = try? container.decode(String.self, forKey: .customChoice)
         self.pollType = try container.decode(Enum.self, forKey: .pollType)
         self.singleChoice = try? container.decode(UInt64.self, forKey: .singleChoice)
     }
