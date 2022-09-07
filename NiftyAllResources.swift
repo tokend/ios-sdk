9671bdded34335fe16223029ce62bd77c36d7b46
Yurii Poberezhnyi <yurapoberezhnyy@gmail.com>
2022-09-07 15:07:50 +0300

    Added models

---
+++ NiftyAllResources.swift
@@ -0,0 +1,26 @@
+// Auto-generated code. Do not edit.
+
+import Foundation
+import DLJSONAPI
+
+extension Nifty {
+enum AllResources {
+    
+    // swiftlint:disable function_body_length
+    public static func registerAllResources() {
+        let allResources: [Resource.Type] = [
+            ReactionResource.self,
+            TitleResource.self
+        ]
+        
+        for res in allResources {
+            Context.registerClass(res)
+        }
+        
+        for res in ManualResources.resources {
+            Context.registerClass(res)
+        }
+    }
+    // swiftlint:enable function_body_length
+}
+}
---
+++ NiftyTitleDetails.swift
@@ -0,0 +1,46 @@
+// Auto-generated code. Do not edit.
+
+import Foundation
+
+// MARK: - TitleDetails
+
+extension Nifty {
+public struct TitleDetails: Decodable {
+    
+    public enum CodingKeys: String, CodingKey {
+        // attributes
+        case additionalImages
+        case fullDescription
+        case location
+        case logo
+        case name
+        case ownershipDocs
+        case propertyValue
+    }
+    
+    // MARK: Attributes
+    
+    public let additionalImages: [File]
+    public let fullDescription: String
+    public let location: String
+    public let logo: File
+    public let name: String
+    public let ownershipDocs: [File]
+    public let propertyValue: String
+
+    // MARK: -
+    
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        self.additionalImages = try container.decode([File].self, forKey: .additionalImages)
+        self.fullDescription = try container.decode(String.self, forKey: .fullDescription)
+        self.location = try container.decode(String.self, forKey: .location)
+        self.logo = try container.decode(File.self, forKey: .logo)
+        self.name = try container.decode(String.self, forKey: .name)
+        self.ownershipDocs = try container.decode([File].self, forKey: .ownershipDocs)
+        self.propertyValue = try container.decode(String.self, forKey: .propertyValue)
+    }
+
+}
+}
