8de483e127fdc34a4960542dc8a7a4a26449a5e9
Ksenia Didusenko <ksenia.didusenko@gmail.com>
2023-03-09 12:50:21 +0200

    Updated filter

--- Sources/JSONAPI/v3/APIs/LiquidityPools/LiquidityPoolsFiltersV3.swift
+++ Sources/JSONAPI/v3/APIs/LiquidityPools/LiquidityPoolsFiltersV3.swift
@@ -4,7 +4,7 @@ public enum LiquidityPoolsFilterOptionV3: FilterOption {
     case asset(_ value: String)
     case lpToken(_ value: String)
     case balancesOwner(_ value: String)
-    case excludedAssets(_ value: [String])
+    case excludedAssets(_ value: String)
 }
 
 public class LiquidityPoolsFiltersV3: RequestFilters <LiquidityPoolsFilterOptionV3> {}
